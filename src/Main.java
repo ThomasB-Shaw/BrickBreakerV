94808add93c7c7ab019f535aba0008996d8e5ef2
Thomas Brookshaw <tbrookshaw13@gmail.com>
2021-02-09 15:38:35 -0600

    init commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+
+import javax.swing.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Vapor Breaker");
+        obj.setResizable(false);
+    }
+}
